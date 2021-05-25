a248f7d8debf4ac23ee1cd7bf5b696d1bd6fa8c0
bbd10160 <riya@bbd.co.za>
2021-05-25 10:40:35 +0530

    layout, header, navigation, home page, etc. done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { DefaultComponent } from './layouts/default/default.component';
+// import { HomeComponent } from './admin/home/home.component';
+import { ManageUserTypeComponent } from './admin/manage-user-type/manage-user-type.component';
+import { Roles } from './constants/roles';
+import { AuthGuard } from './auth.guard';
+// import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+// import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+  // { path: 'admin/login', component: AdminLoginComponent },
+  // { path: 'admin/home', component: AdminHomeComponent },
+  {
+    path: '', component: DefaultComponent, children: [{
+      path: 'login', component: LoginComponent
+    }, {
+      path: 'home', component: HomeComponent
+    }, {
+      path: 'admin/login', component: AdminLoginComponent
+    }, { 
+      path: 'admin/dashboard', component: DashboardComponent, canActivate: [AuthGuard], data: { role: Roles.ADMIN } 
+    }, { 
+      path: 'admin/manage-member', component: ManageUserTypeComponent, canActivate: [AuthGuard], data: { role: Roles.ADMIN } 
+    }]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
+import { AdminComponent } from './admin/admin.component';
+// import { AdminModule } from './admin/admin.module';
+import { DefaultModule } from './layouts/default/default.module';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { AuthGuard } from './auth.guard';
+import { AuthenticationService } from './_services/authentication.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+// import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ManageUserTypeComponent } from './admin/manage-user-type/manage-user-type.component';
+// import { HomeComponent } from './admin/adminHome/home.component';
+import { NotifierComponent } from './notifier/notifier.component';
+import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // AdminHomeComponent,
+    NavComponent,
+    ManageUserTypeComponent,
+    // HomeComponent,
+    AdminComponent,
+    NotifierComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    DefaultModule
+    // AdminModule
   ],
-  providers: [],
+  providers: [AuthenticationService, AuthGuard,
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
